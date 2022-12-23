4a1577856e6909c79f83ae224dee53fd1b5ba65f
Xavier Lemmuele C Romano <54741558+AlphaXexia@users.noreply.github.com>
2022-12-24 06:20:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 import os
-import tempfile
-import requests
-from bs4 import BeautifulSoup
-from google.cloud import texttospeech
+import openai
 
-# Set up the Cloud Text-to-Speech client
-client = texttospeech.TextToSpeechClient()
+openai.api_key = "YOUR_API_KEY"
 
-# Set up the synthesis input
-synthesis_input = texttospeech.types.SynthesisInput(text='\n'.join(responses))
+def synthesize_voice(text):
+    prompt = (
+        f"Please play the following message: {text}"
+    )
+    completions = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    message = completions.choices[0].text
+    message = message.strip()
 
-# Set up the voice synthesis parameters
-voice = texttospeech.types.VoiceSelectionParams(
-    language_code='en-US',
-    ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+    # Save the synthesized audio to a file
+    with open("voice.mp3", "wb") as f:
+        f.write(completions.choices[0].audio)
 
-# Set up the audio synthesis parameters
-audio_config = texttospeech.types.AudioConfig(
-    audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+def main():
+    # Read the message to be synthesized from stdin
+    text = input()
+    synthesize_voice(text)
 
-# Synthesize the voice using the main.py Python script
-response = client.synthesize_speech(synthesis_input, voice, audio_config)
-
-# Write the synthesized voice to a temporary file
-with tempfile.NamedTemporaryFile() as f:
-    f.write(response.audio_content)
-    f.flush()
-    # Play the synthesized voice using the system's default media player
-    os.system(f"open {f.name}")
+if __name__ == "__main__":
+    main()
