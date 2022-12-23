f1a2dbc7d81d76f912f9fac262ee7aa345948101
Xavier Lemmuele C Romano <54741558+AlphaXexia@users.noreply.github.com>
2022-12-24 07:38:35 +0800

    Update background-script.js

--- background/background-script.js
+++ background/background-script.js
@@ -31,10 +31,11 @@ chrome.storage.onChanged.addListener((changes, namespace) => {
     }
   }
 });
+
 // Listen for response messages from the chat page
 function listenForResponses() {
   chrome.webRequest.onBeforeRequest.addListener(
-    function(details) { // <-- define the function as an anonymous function here
+    (details) => {
       // Check if the request is a response message
       if (details.url.includes('/api/v1/messages')) {
         // Get the message text from the request body
@@ -56,18 +57,18 @@ function stopListeningForResponses() {
 // Extract the message text from a request body object
 function getMessageFromRequestBody(requestBody) {
   // Parse the request body as a JSON object
-  const body = JSON.parse(String.fromCharCode.apply(null, requestBody));
-  // Return the 'text' property from the body object
-  return body.text;
+const body = JSON.parse(String.fromCharCode.apply(null, requestBody));
+// Return the 'text' property from the body object
+return body.text;
 }
 
 // Synthesize the voice for a message using the main.py script
 function synthesizeVoice(message) {
-  // Run the main.py script with the message text as an argument
-  chrome.runtime.sendNativeMessage(
-    'openai_voice_extension',
-    { message: message },
-    (response) => {
+// Run the main.py script with the message text as an argument
+chrome.runtime.sendNativeMessage(
+'openai_voice_extension',
+{ message: message },
+(response) => {
 // Handle the response from the main.py script
 if (chrome.runtime.lastError) {
 console.error(chrome.runtime.lastError.message);
