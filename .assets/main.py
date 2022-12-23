461d72af4c784a4f6f49535cda923271d56cb618
Xavier Lemmuele C Romano <54741558+AlphaXexia@users.noreply.github.com>
2022-12-23 19:37:10 +0800

    Update main.py

--- .assets/main.py
+++ .assets/main.py
@@ -1,14 +1,21 @@
 import os
 import tempfile
 
-from engine import responses
+from engine import get_response
+from grabber import get_url
 from google.cloud import texttospeech
+
+# Get the current URL
+url = get_url()
+
+# Get the Assistant's response from the URL
+response = get_response(url)
 
 # Set up the Cloud Text-to-Speech client
 client = texttospeech.TextToSpeechClient()
 
 # Set up the synthesis input
-synthesis_input = texttospeech.types.SynthesisInput(text='\n'.join(responses))
+synthesis_input = texttospeech.types.SynthesisInput(text=response)
 
 # Set up the voice synthesis parameters
 voice = texttospeech.types.VoiceSelectionParams(
