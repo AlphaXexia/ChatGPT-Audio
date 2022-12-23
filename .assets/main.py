c1e0716b290f454de2df9cafdf0b93612a362d1c
Xavier Lemmuele C Romano <54741558+AlphaXexia@users.noreply.github.com>
2022-12-23 19:43:29 +0800

    Update main.py
    No longer uses grabber.py since it relies on external input for the URL it processes. This reduces the risk of an attacker manipulating the URL that is delivered to the main.py script, as the URL is now hardcoded in the script itself.

--- .assets/main.py
+++ .assets/main.py
@@ -1,21 +1,14 @@
 import os
 import tempfile
 
-from engine import get_response
-from grabber import get_url
+from engine import responses
 from google.cloud import texttospeech
-
-# Get the current URL
-url = get_url()
-
-# Get the Assistant's response from the URL
-response = get_response(url)
 
 # Set up the Cloud Text-to-Speech client
 client = texttospeech.TextToSpeechClient()
 
 # Set up the synthesis input
-synthesis_input = texttospeech.types.SynthesisInput(text=response)
+synthesis_input = texttospeech.types.SynthesisInput(text='\n'.join(responses))
 
 # Set up the voice synthesis parameters
 voice = texttospeech.types.VoiceSelectionParams(
