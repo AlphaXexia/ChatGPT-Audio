42adabfdd3309acee799f9e8abe0f1bec0867d69
Xavier Lemmuele C Romano <54741558+AlphaXexia@users.noreply.github.com>
2022-12-23 18:47:43 +0800

    make this change on file

---
+++ .assets/main.py
@@ -0,0 +1,30 @@
+import os
+import tempfile
+
+from engine import responses
+from google.cloud import texttospeech
+
+# Set up the Cloud Text-to-Speech client
+client = texttospeech.TextToSpeechClient()
+
+# Set up the synthesis input
+synthesis_input = texttospeech.types.SynthesisInput(text='\n'.join(responses))
+
+# Set up the voice synthesis parameters
+voice = texttospeech.types.VoiceSelectionParams(
+    language_code='en-US',
+    ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+
+# Set up the audio synthesis parameters
+audio_config = texttospeech.types.AudioConfig(
+    audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+# Synthesize the voice
+response = client.synthesize_speech(synthesis_input, voice, audio_config)
+
+# Write the synthesized voice to a temporary file
+with tempfile.NamedTemporaryFile() as f:
+    f.write(response.audio_content)
+    f.flush()
+    # Play the synthesized voice using the system's default media player
+    os.system(f"open {f.name}")
